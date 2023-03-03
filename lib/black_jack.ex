3e39dd3ffa435995764ef012e13292fa3eef11b1
AndersonElit <rodriarri@gmail.com>
2023-03-03 12:21:17 -0500

    calcular ganador

--- lib/black_jack.ex
+++ lib/black_jack.ex
@@ -2,26 +2,34 @@ defmodule BlackJack do
 
   @cartas ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
   @valores %{"2" => 2, "3" => 3, "4" => 4, "5" => 5, "6" => 6, "7" => 7, "8" => 8, "9" => 9, "10" => 10, "J" => 10, "Q" => 10, "K" => 10, "A" => 11}
-  @simbolos ["♠", "♥", "♦", "♣"]
+  #@simbolos ["♠", "♥", "♦", "♣"]
 
   def empezar do
-    #robar_carta?()
     IO.puts("¿como te llamas?")
-    nombre_jugador = IO.gets("")
-    jugador1 = %{"nombre" => nombre_jugador, "puntaje" => 0}
-    jugador2 = %{"nombre" => "PC", "puntaje" => 0}
-    nueva_ronda(jugador1, jugador2)
-    #IO.puts(jugador1)
-    #comparar_puntajes(Map.get(jugador1, "puntaje"), Map.get(jugador2, "puntaje"))
+    jugador1 = IO.gets("")
+    jugador2 = "PC"
+    puntaje_jugador_1 = []
+    puntaje_jugador_2 = []
+    {puntaje_jugador_1, puntaje_jugador_2} = ronda(jugador1, puntaje_jugador_1, jugador2, puntaje_jugador_2)
+
+    IO.puts(Enum.sum(puntaje_jugador_1))
+    IO.puts(Enum.sum(puntaje_jugador_2))
+
   end
 
-  def nueva_ronda(jugador1, jugador2) do
-    carta_jugador = obtener_carta()
-    carta_pc = obtener_carta()
-    #IO.puts("carta " <> Map.get(jugador1, "nombre") <> " = " <> carta_jugador <> Enum.random(@simbolos))
-    #IO.puts("carta " <> Map.get(jugador2, "nombre") <> " = " <> carta_pc <> Enum.random(@simbolos))
-    IO.puts("carta " <> Map.get(jugador1, "nombre") <> " = " <> carta_jugador)
-    IO.puts("carta " <> Map.get(jugador2, "nombre") <> " = " <> carta_pc)
+  def ronda(jugador1, puntaje1, jugador2, puntaje2) do
+    carta1 = obtener_carta()
+    carta2 = obtener_carta()
+    IO.puts("carta " <> jugador1 <> " = " <> carta1)
+    IO.puts("carta " <> jugador2 <> " = " <> carta2)
+    nuevo_puntaje_1 = puntaje1 ++ [Map.get(@valores, carta1)]
+    nuevo_puntaje_2 = puntaje2 ++ [Map.get(@valores, carta2)]
+    if robar_carta?() do
+      ronda(jugador1, nuevo_puntaje_1, jugador2, nuevo_puntaje_2)
+    else
+      comparar_puntajes(nuevo_puntaje_1, nuevo_puntaje_2)
+      {nuevo_puntaje_1, nuevo_puntaje_2}
+    end
   end
 
   def obtener_carta do
